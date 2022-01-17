39b55b9fd0a4777b3e949d0129f4f67ba8d734e3
Divyashree Agrawal <64089485+Divyashree356@users.noreply.github.com>
2022-01-17 19:59:54 +0530

    Create 111. Minimum Depth of Binary Tree.cpp

---
+++ 111. Minimum Depth of Binary Tree.cpp
@@ -0,0 +1,35 @@
+Input: root = [3,9,20,null,null,15,7]
+Output: 2
+
+Input: root = [2,null,3,null,4,null,5,null,6]
+Output: 5
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root==NULL)
+            return 0;
+        
+        if(root->left==NULL && root->right==NULL)
+            return 1;
+        
+        if(!root->left)
+            return minDepth(root->right)+1;
+        
+        if(!root->right)
+            return minDepth(root->left)+1;
+        
+        return min(minDepth(root->left),minDepth(root->right))+1;
+    }
+};
