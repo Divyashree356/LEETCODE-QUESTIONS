d0bbc42cc2eaca3f6167e32792de444dd64de27b
Divyashree Agrawal <64089485+Divyashree356@users.noreply.github.com>
2022-01-26 19:28:09 +0530

    Create #48. Rotate Image.java

---
+++ #48. Rotate Image.java
@@ -0,0 +1,49 @@
+          #MEDIUM
+
+Example 1:
+
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]]
+Example 2:
+
+
+Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+
+
+class Solution {
+    public void transpose(int[][] arr)
+    {
+        int n=arr.length;
+        
+        for(int i=0;i<n;i++)
+        {
+          for(int j=i;j<n;j++)
+          {
+              int t=arr[i][j] ;
+              arr[i][j]=arr[j][i];
+              arr[j][i]=t;
+          }
+        }
+    }
+public void reverse(int[][] arr)
+{
+    int n=arr.length;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n/2;j++)
+        {
+         int t= arr[i][j];
+            arr[i][j]=arr[i][n-1-j];
+            arr[i][n-1-j]=t;
+    }
+    }
+    
+}
+    public void rotate(int[][] arr) {
+        transpose(arr);
+        reverse(arr);
+
+    }
+}
