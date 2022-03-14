df39b6b7bd8caff382c330e8788605bd10835da8
Divyashree Agrawal <64089485+Divyashree356@users.noreply.github.com>
2022-03-14 12:56:56 +0530

    Create #1669. Merge In Between Linked Lists.java

---
+++ #1669. Merge In Between Linked Lists.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        ListNode start = list1;
+        int i=1;
+        while(i<a)
+        {
+            start= start.next;
+            i++;
+        }
+        ListNode end= start;
+        i=a;
+        while(i<=b)
+        {
+            end= end.next;
+            i++;
+        }
+        start.next= list2;
+        while(list2.next!=null)
+        {
+            list2= list2.next;
+        }
+        list2.next= end.next;
+        return list1;
+    }
+}
