820e809fe66e9a0fab8dfb306b5e9d98231b1bca
Divyashree Agrawal <64089485+Divyashree356@users.noreply.github.com>
2022-03-26 20:21:31 +0530

    Update #326. Power of Three.java

--- #326. Power of Three.java
+++ #326. Power of Three.java
@@ -1,15 +1,16 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
        
+        //n=27
         if(n==0)
             return false;
         
-        while(n%3==0)
+        while(n%3==0)  //n=9 //0 //loop break
         {
-            n/=3;
+            n/=3;  //n=3 //n=1
         }
         
-        if(n==1)
+        if(n==1)  // 1 return 
             return true;
         else
             return false;
