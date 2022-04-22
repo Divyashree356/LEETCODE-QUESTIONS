f5cc42074e66bd2392d077f80759dd0241e6f241
Divyashree Agrawal <64089485+Divyashree356@users.noreply.github.com>
2022-04-22 19:13:24 +0530

    Create #343. Integer Break.java

---
+++ #343. Integer Break.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int integerBreak(int n) {
+       
+        int[] dp = new int[n+1];
+        dp[0]=1;
+        int max=0;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=i;j<=n;j++)
+            {
+                dp[j]= Math.max(dp[j] , dp[j-i]*i);
+                if(j==n && max<dp[j])
+                {
+                    max=dp[j];
+                }
+            }
+        }
+        return max;
+    }
+}
