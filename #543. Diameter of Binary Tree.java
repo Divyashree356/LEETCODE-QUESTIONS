28b7177ee6b824119e54fb875aece7fc205a1f3c
Divyashree Agrawal <64089485+Divyashree356@users.noreply.github.com>
2022-05-06 12:58:31 +0530

    Update #543. Diameter of Binary Tree.java

--- #543. Diameter of Binary Tree.java
+++ #543. Diameter of Binary Tree.java
@@ -13,7 +13,10 @@
  *     }
  * }
  */
+
+#SOLUTION-1
 class Solution {
+  
     private int getHeight(TreeNode root)
     {
         if(root==null)
@@ -40,5 +43,67 @@ class Solution {
         
         return Math.max(mD, Math.max(rd, ld));
         
+    }
+  }
+
+#SOLUTION-2
+class Solution {
+    public class pair
+    {
+        int h;
+        int d;
+        
+        pair(){}
+        pair(int h , int d)
+        {
+            this.h=h;
+            this.d=d;
+        }
+    }
+    
+    private pair diameter(TreeNode node)
+    {
+        if(node==null)
+            return new pair(-1,0);
+        
+        pair la=diameter(node.left);
+        pair ra=diameter(node.right);
+        
+         pair my= new pair();
+        
+         my.h= Math.max(la.h,ra.h)+1;
+         int acrossR= la.h + ra.h +2;
+        
+        my.d= Math.max(acrossR, Math.max(la.d, ra.d));
+        return my;
+        
+    }
+    public int diameterOfBinaryTree(TreeNode root) {
+        pair ans= diameter(root);
+        return ans.d;
+    }
+}
+
+#SOLUTION-3
+class Solution {
+    
+    public int[] diameter(TreeNode node)
+    {
+        if(node==null)
+             return new int[]{-1,0};
+        
+        int[] la= diameter(node.left);
+        int[] ra=diameter(node.right);
+        
+        int[] my= new int[2];
+        my[0] = Math.max(la[0] ,ra[0])+1;
+        int acrossR= la[0]+ra[0]+2;
+        
+        my[1]= Math.max(acrossR , Math.max(la[1], ra[1]));
+        return my;
+    }
+    public int diameterOfBinaryTree(TreeNode root) {
+        int[] ans=  diameter(root);
+        return ans[1];
     }
 }
